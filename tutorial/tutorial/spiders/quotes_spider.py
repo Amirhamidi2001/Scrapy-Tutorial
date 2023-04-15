7c96966d21cda0f46acfcd797e7c9ea4c909230f
amirhamidi2001 <amirarsalanh2001@gmail.com>
2023-04-15 10:21:02 +0330

    Following links
    now our spider modified to recursively follow the link to the next page, extracting data from it

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -5,7 +5,6 @@ class QuotesSpider(scrapy.Spider):
     name = "quotes"
     start_urls = [
         'https://quotes.toscrape.com/page/1/',
-        'https://quotes.toscrape.com/page/2/',
     ]
 
     def parse(self, response):
@@ -15,3 +14,8 @@ class QuotesSpider(scrapy.Spider):
                 'author': quote.css('small.author::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
