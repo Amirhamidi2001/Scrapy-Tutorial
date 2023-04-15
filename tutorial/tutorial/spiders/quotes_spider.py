9fe0c7ab4443aa2e43f1d5f22a716b0f1ad5602c
amirhamidi2001 <amirarsalanh2001@gmail.com>
2023-04-15 10:41:27 +0330

    Using spider arguments
    You can provide command line arguments to your spiders by using the -a option when running them

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -3,18 +3,21 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    start_urls = [
-        'https://quotes.toscrape.com/page/1/',
-    ]
+
+    def start_requests(self):
+        url = 'https://quotes.toscrape.com/'
+        tag = getattr(self, 'tag', None)
+        if tag is not None:
+            url = url + 'tag/' + tag
+        yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').get(),
-                'author': quote.css('span small::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
+                'author': quote.css('small.author::text').get(),
             }
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
-            yield response.follow(next_page, callback=self.parse)
+            yield response.follow(next_page, self.parse)
