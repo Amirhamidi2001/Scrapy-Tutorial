885c5781b9ea5ec194a1194ac2a27313ba1e88ca
amirhamidi2001 <amirarsalanh2001@gmail.com>
2023-04-15 10:23:58 +0330

    A shortcut for creating Requests
    As a shortcut for creating Request objects you can use response.follow

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -11,11 +11,10 @@ class QuotesSpider(scrapy.Spider):
         for quote in response.css('div.quote'):
             yield {
                 'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
+                'author': quote.css('span small::text').get(),
                 'tags': quote.css('div.tags a.tag::text').getall(),
             }
 
         next_page = response.css('li.next a::attr(href)').get()
         if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+            yield response.follow(next_page, callback=self.parse)
