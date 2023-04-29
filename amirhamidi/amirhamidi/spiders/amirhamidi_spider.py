62b08f69f6309b05140b915050ee8040ac31da8e
amirhamidi2001 <amirarsalanh2001@gmail.com>
2023-04-29 12:50:56 +0330

    Change spider
    Data extraction and stored in jl format

--- amirhamidi/amirhamidi/spiders/amirhamidi_spider.py
+++ amirhamidi/amirhamidi/spiders/amirhamidi_spider.py
@@ -26,13 +26,37 @@
 # Data extraction and stored in json format
 # Running the project with command: scrapy crawl amirhamidi -O amirhamidi.json
 
+# import scrapy
+
+
+# class AmirhamidiSpider(scrapy.Spider):
+#     name = "amirhamidi"
+#     allowed_domains = ["amirhamidi.pythonanywhere.com"]
+#     start_urls = ["http://amirhamidi.pythonanywhere.com/"]
+
+#     def parse(self, response):
+
+#         for href in response.xpath('//a/@href').getall():
+#             yield {"title": href}
+
+
+
+
+
+
+
+# Data extraction and stored in jl format
+# Running the project with command: scrapy crawl amirhamidi -O amirhamidi.jl
+
 import scrapy
 
 
 class AmirhamidiSpider(scrapy.Spider):
     name = "amirhamidi"
     allowed_domains = ["amirhamidi.pythonanywhere.com"]
-    start_urls = ["http://amirhamidi.pythonanywhere.com/"]
+
+    def start_requests(self):
+        yield scrapy.Request("http://amirhamidi.pythonanywhere.com/", self.parse)
 
     def parse(self, response):
 
